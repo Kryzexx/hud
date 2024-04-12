fbb9abe4993e195e58773047a7596922711e4686
a.m <126760638+Kryzexx@users.noreply.github.com>
2024-04-12 13:18:01 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
-/*cef.emit("game:hud:setComponentVisible", "interface", false);
+cef.emit("game:hud:setComponentVisible", "interface", false);
 cef.emit("game:hud:setComponentVisible", "radar", true);
 cef.emit("game:data:pollPlayerStats", true, 50);
 
 cef.on("game:data:playerStats", (hp, max_hp, arm, breath, wanted, weapon, ammo, max_ammo, money, speed) => {
-    document.querySelector(".hprogress").style.width = hp + "%";
-    document.querySelector(".ahprogress").style.width = arm + "%";
-    document.getElementById(".gun span").textContent = `${weapon}`;
-    document.getElementById(".ammo-counter span:nth-child(1)").innerText = `${ammo}`;
-    document.getElementById(".ammo-counter span:nth-child(2)").innerText = `/${max_ammo}`;
+    document.getElementById("hprogress").style.width = hp + "%";
+    document.getElementById("aprogress").style.width = arm + "%";
+    if(weapon == 31) {
+        document.getElementById("gun").textContent = `M4`;
+    }
+    else if(weapon == 24) {
+        document.getElementById("gun").textContent = `Deagle`;
+    }
+    else if(weapon == 30) {
+        document.getElementById("gun").textContent = `AK-47`;
+    }
+    else if(weapon == 25) {
+        document.getElementById("gun").textContent = `Shotgun`;
+    }
+    else if(weapon == 28) {
+        document.getElementById('gun').textContent = `Uzi`
+    }
+    else if(weapon == 32) {
+        document.getElementById('gun').textContent = `Tec-9`
+    }
+    else if(weapon == 33) {
+        document.getElementById('gun').textContent = `Rifle`
+    }
+    else if(weapon == 38) {
+        document.getElementById('gun').textContent = `Minigun`
+    }
+    for(i = 0; i <= 15; i++) {
+        if(weapon == i) {
+            document.getElementById('gun').textContent = 'Fist'
+        }
+    }
+    document.getElementById("ammo").innerText = `${ammo}`;
+    document.getElementById("max-ammo").innerText = `/${max_ammo}`;
 });
 
 
@@ -27,4 +55,4 @@ cef.on("data:pool:fail", () => {
         document.querySelector('.fail').classList.remove('anim')
         document.querySelector('.fail').style.opacity = 0
     }, 2000);
-}) */
+}) 
