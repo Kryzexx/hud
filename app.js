235c6082482e3b27e933c0f8ca3afc397915920c
a.m <126760638+Kryzexx@users.noreply.github.com>
2024-04-12 12:26:34 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 cef.emit("game:hud:setComponentVisible", "interface", false);
 cef.emit("game:hud:setComponentVisible", "radar", true);
 cef.emit("game:data:pollPlayerStats", true, 50);
-cef.emit("pwd:try", true, 50);
+cef.emit("data:try", true, 50);
 
 cef.on("game:data:playerStats", (hp, max_hp, arm, breath, wanted, weapon, ammo, max_ammo, money, speed) => {
-    document.querySelector(".aprogress").style.width = hp;
-    document.querySelector(".hprogress").innerText = arm;
-    document.getElementById(".gun span").innerText = `${weapon}`;
+    document.querySelector(".hprogress").style.width = hp + "%";
+    document.querySelector(".ahprogress").style.width = arm + "%";
+    document.getElementById(".gun span").textContent = `${weapon}`;
     document.getElementById(".ammo-counter span:nth-child(1)").innerText = `${ammo}`;
     document.getElementById(".ammo-counter span:nth-child(2)").innerText = `/${max_ammo}`;
 });
 
-cef.on("pwd:success", () => {
+cef.on("data:success", () => {
     document.querySelector('.success').style.animation = "slide-left 0.9s ease 1"
     setTimeout(() => {
         document.querySelector('.success').style.animation = ""
@@ -19,7 +19,7 @@ cef.on("pwd:success", () => {
     }, 2000);
 }) 
 
-cef.on("pwd:fail", () => {
+cef.on("data:fail", () => {
     document.querySelector('.fail').style.animation = "slide-left 0.9s ease 1"
     setTimeout(() => {
         document.querySelector('.fail').style.animation = ""
